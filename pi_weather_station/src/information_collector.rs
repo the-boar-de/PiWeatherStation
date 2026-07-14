b24a03f032973027f70ad629ade6c4693d4a1310
The_Boar_DE <116942008+the-boar-de@users.noreply.github.com>
2026-07-14 14:10:06 +0200

    Enhance information_collector with documentation comments
    Added documentation comments to the information_collector struct and its implementation.

--- pi_weather_station/src/information_collector.rs
+++ pi_weather_station/src/information_collector.rs
@@ -1,14 +1,23 @@
- //Lib Use
- 
- pub struct information_collector{
+/// Description
+/// The Information collector is the collector for the API from the OpenWeatherMap and puts it in the data format that is use in this project.
+/// The collector requested the Information form the API after a setted time.
+/// Information of the Location ( the location of the device ) is based on from where the request to the API comes. 
+/// 
 
+// ------
+// Lib use
+
+/// Information collector
+ pub struct information_collector{
+ // mutable variables
+  
 
 }
 
 impl information_collector{
 
 
-
+/// functions
 
     
 }
